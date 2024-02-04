73f6d20827feefd8855833f0f368121cf739d455
plana-planetarium <3412294524@qq.com>
2024-02-04 20:13:10 +0800

    update

--- change.py
+++ change.py
@@ -28,16 +28,15 @@ for name in name_list:
 text_git = \
     '      - name: Git push log\n' + \
     '        run: |\n' + \
-    '          mv ./downloads_log ../\n' + \
-    '          cd ..\n' + \
-    '          rm -rf ./pixiv_sync\n' + \
+    '          mkdir ../push_resp\n' + \
+    '          mv ./downloads_log ../push_resp/\n' + \
+    '          cd ../push_resp\n' + \
     '          git clone https://github.com/plana-planetarium/pixiv_sync.git\n' + \
     '          git config --global user.name "plana-planetarium"\n' + \
     '          git config --global user.email "3412294524@qq.com"\n' + \
     '          cd pixiv_sync\n' + \
     '          rm -rf downloads_log\n' + \
     '          mv ../downloads_log ./\n' + \
-    '          rm -rf .git\n' + \
     '          git init\n' + \
     '          git remote add origin git@github.com:plana-planetarium/pixiv_sync.git\n' + \
     '          git add ./downloads_log\n' + \
