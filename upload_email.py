44a58df85df651cc11783c51edf1a756ef2ffb4c
plana-planetarium <3412294524@qq.com>
2024-02-05 10:55:15 +0800

    update

--- upload_email.py
+++ upload_email.py
@@ -19,7 +19,7 @@ i = 1
 
 email_url = ''
 while i <= len(text)-5:
-    email_url += text[i] + '\n'
+    email_url += text[i]
     i += 1
 
 email_tool(receiver = text[0], URL = email_url, upload_name = upload_name, status_num = text[-3], name = text[-2], user_update = text[-1])
